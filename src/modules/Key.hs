9865f45136d07fb32931282579d45b756a6a3b75
martinvelezf <martinvelezf@hotmail.com>
2020-05-21 17:51:16 -0500

    newdata

--- src/modules/Key.hs
+++ src/modules/Key.hs
@@ -5,8 +5,11 @@ data Count= Count Int Int Int Int deriving Show
 setCount::Int->Int->Int->Int->Count
 setCount =Count 
 
---data Word=Word8 8 | Word16 16 | Word32 32 | Word64 64 deriving (Read,Show,Eq)
---data Word= Word 8 |  16 |  32 | 64 
+data BigCount= BigCount Count [Int] [Int]
+setBigCount::Count->[Int]->[Int]->BigCount
+setBigCount=BigCount
+getSecret::BigCount->[Int]
+getSecret (BigCount _ _ x)=x
 data Word=Word8 | Word16  | Word32  | Word64  deriving (Read,Show,Eq)
 
 w :: Key.Word->Int
@@ -67,19 +70,18 @@ initializateS pw qw size= skey where
 
 
 
-fsmixed ::Int->Int -> (Count, ([Int],[Int])) -> (Count, ([Int],[Int]))
-fsmixed t c (Count a b i j,(l,s))= x where     
+fsmixed ::Int->Int -> BigCount -> BigCount
+fsmixed t c (BigCount (Count a b i j) l s) = x where     
         a'=(s!!i+(a+b)) `B.rotateL` 3
         b'=(l!!j+(a'+b)) `B.rotateL` (a'+b)
         s'=changevalue s i a'
         l'=changevalue l j b'
         i'=(i+1)`mod` t
         j'=(j+1)`mod` c
-        x=(setCount a' b' i' j',(l',s'))
-
+        x=(setBigCount (setCount a' b' i' j') l' s')
 
 smixed :: [Int] -> [Int] -> Int -> Int -> [Int]
-smixed l s t c=  snd $snd $iterate (fsmixed t c) (Count 0 0 0 0 ,(l,s))!! (3*max t c)
+smixed l s t c=  getSecret  $iterate (fsmixed t c) (BigCount (Count 0 0 0 0 ) l s)!! (3*max t c)
 
 createSecretKey :: Key.Word -> [Int] -> Int -> [Int]
 createSecretKey word k r=x where
@@ -93,3 +95,5 @@ createSecretKey word k r=x where
     t=2*(r+1)
     s=initializateS pw qw t
     x=smixed l s t c
+
+
